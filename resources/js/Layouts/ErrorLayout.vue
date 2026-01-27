9ea091ae6defbaa3b457e6e69361f8a5f37028bb
Dmitry Beznosiuk <Dmitrybeznosiukcode@gmail.com>
2026-01-27 19:56:52 +0200

    add 404 error on project

---
+++ resources/js/Layouts/ErrorLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    title: {
+        type: String,
+        default: 'Error',
+    },
+});
+</script>
+
+<template>
+    <div class="min-h-screen bg-gradient-to-br from-gray-50 via-blue-50 to-gray-50">
+        <Head :title="title || 'Error'" />
+        <slot />
+    </div>
+</template>
